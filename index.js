c8ea408a863b319ec6074dc6d32f375459917b7d
Zacharia <ndunguzachary24@gmail.com>
2022-06-13 23:50:40 +0300

    commit tree e1aea28281f27d08d4a255cc40eb330ec34d3f8e

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-function addingEventListener() {
+
+
+
+function addingEventListener(){
+    let input = document.getElementById('input')
+    input.addEventListener('mouseenter', () =>{
+        alert('HI, Write Something')
+    } )
 }
+addingEventListener()
